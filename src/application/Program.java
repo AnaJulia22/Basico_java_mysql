c39d70e24b5e72f86080bf2bb29708263a4e74c5
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-10-05 03:32:41 -0300

    connecting to mysql

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,12 @@
 package application;
 
 import java.util.Locale;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import java.util.Scanner;
 
 import entities.Product;
@@ -8,6 +14,33 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://localhost:4306/java_curso";
+		String username = "root";
+		String password = "";
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection connection = DriverManager.getConnection(url, username, password);
+			
+			Statement statement = connection.createStatement();
+			
+			
+			ResultSet resultSet = statement.executeQuery("select * from produto");
+			
+			while(resultSet.next()) {
+				System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2)+ " " + resultSet.getDouble(3) + " " + resultSet.getInt(4));
+			}
+			
+			connection.close();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
